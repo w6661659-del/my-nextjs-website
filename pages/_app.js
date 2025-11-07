cb58290ab8a3409000f62a13393e38e0b3f1ca3d
w6661659-del <w6661659@gmail.com>
2025-11-07 15:53:58 -0800

    fix

--- pages/_app.js
+++ pages/_app.js
@@ -4,6 +4,12 @@ import { ThemeProvider } from "next-themes";
 function MyApp({ Component, pageProps }) {
   return (
     <ThemeProvider attribute="class">
+          <ThemeProvider
+      attribute="class"
+      defaultTheme="light"   // 👈 always start with light
+      enableSystem={false}   // 👈 ignore OS preference
+      disableTransitionOnChange
+    ></ThemeProvider>
       <Component {...pageProps} />
     </ThemeProvider>
   );
